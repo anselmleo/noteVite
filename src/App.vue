62f0f39cd8f717433279cc27097228fed81a2307
Anselm <anselm@users-MacBook-Pro.local>
2023-12-07 08:47:09 +0100

    style note cards

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,10 @@
         <button>+</button>
       </header>
       <div class="cards-container">
-        <p class="main-text"></p>
-        <p class="date"></p>
+        <div class="card">
+          <p class="main-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta numquam expedita laudantium commodi saepe incidunt deleniti reiciendis eum aliquid qui.</p>
+          <p class="date">12-12-2023</p>
+        </div>
       </div>
     </div>
   </main>
@@ -17,6 +19,50 @@
   main {
     width: 100vw;
     height: 100vh;
-    background-color: skyblue
+    background-color: white
+  }
+
+  .container {
+    max-width: 1000px;
+    padding: 10px;
+    margin: 0 auto
+  }
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 30px 0 30px 0;
+  }
+
+  header button {
+    font-size: xx-large;
+    width: 40px;
+    height: 40px;
+    border-color: black;
+    border-radius: 100%;
+    color: white;
+    background-color: black;
+  }
+
+  .cards-container {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .card {
+    width: 200px;
+    height: 250px;
+    border-radius: 5%;
+    background-color: orange;
+    margin-right: 10px;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  .date { 
+    font-weight: bold;
   }
 </style>
