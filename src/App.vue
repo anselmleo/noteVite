889a1ec201ed47c3b878f5ca516a3c8905442942
Anselm <anselm@users-MacBook-Pro.local>
2023-12-07 05:16:23 +0100

    setup html markup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>+</button>
+      </header>
+      <div class="cards-container">
+        <p class="main-text"></p>
+        <p class="date"></p>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    width: 100vw;
+    height: 100vh;
+    background-color: skyblue
+  }
+</style>
